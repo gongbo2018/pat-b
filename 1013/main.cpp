04105d88aaab07e50a57d324f36c2f7ce22cf5da
guocheng.zeng <guocheng.zeng@ucloud.cn>
2020-09-01 19:40:53 +0800

    1013 commit

---
+++ 1013/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+const int maxn = 1e6 + 10;
+int prime[maxn], num = 0;
+bool p[maxn] = {0};
+
+void Find_prime(int n) {
+	for (int i = 2; i < maxn; i++) {
+		if (p[i] == false) {
+			prime[num++] = i;
+			if (num >= n) break;
+			for (int j = i + i; j < maxn; j += i) {
+				p[j] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	int m, n, count = 0;
+	scanf("%d%d", &m, &n);
+	Find_prime(n);
+	for (int i = m; i <= n; i++) {
+		printf("%d", prime[i - 1]);
+		count ++;
+		if (count % 10 != 0 && i < n) printf(" ");
+		else printf("\n");
+	}
+
+	return 0;
+}
